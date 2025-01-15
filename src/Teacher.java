4ea2f2b29463287b4da0eed08258927915303477
Alizhan-T <a.toleuxan06@mail.ru>
2025-01-15 08:55:10 +0500

    Update Teacher.java

--- src/Teacher.java
+++ src/Teacher.java
@@ -1,40 +1,28 @@
-public class Teacher {
-    private String name;
+public class Teacher extends Person {
     private String subject;
-    private int experience;
 
-    public Teacher(String name, String subject, int experience) {
-        this.name = name;
+    // Constructor
+    public Teacher(String name, int age, String subject) {
+        super(name, age);
         this.subject = subject;
-        this.experience = experience;
     }
 
-    public String getName() {
-        return name;
-    }
-
+    // Getter and setter
     public String getSubject() {
         return subject;
-    }
-
-    public int getExperience() {
-        return experience;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public void setSubject(String subject) {
         this.subject = subject;
     }
 
-    public void setExperience(int experience) {
-        this.experience = experience;
+    @Override
+    public String getDetails() {
+        return "Teacher: " + toString() + ", Subject: " + subject;
     }
 
     @Override
     public String toString() {
-        return "Teacher(Name: " + name + ", Subject: " + subject + ", Experience: " + experience + " years)";
+        return super.toString() + ", Subject: " + subject;
     }
 }
