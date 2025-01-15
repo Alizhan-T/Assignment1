9806514ff3272ec3486d10118c9c28aab239bcdd
Alizhan-T <a.toleuxan06@mail.ru>
2025-01-15 08:53:58 +0500

    Update Person.java

--- src/Person.java
+++ src/Person.java
@@ -1,1 +1,52 @@
+import java.util.Objects;
 
+// Abstract base class for all people
+abstract class Person {
+    private String name;
+    private int age;
+
+    // Constructor
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    // Getters and setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age < 0) throw new IllegalArgumentException("Age cannot be negative");
+        this.age = age;
+    }
+
+    // Abstract method to get details
+    public abstract String getDetails();
+
+    @Override
+    public String toString() {
+        return "Name: " + name + ", Age: " + age;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Person person = (Person) obj;
+        return age == person.age && Objects.equals(name, person.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+}
